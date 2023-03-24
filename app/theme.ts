2834ac61d23f478771a7aa55c8e081fe48f662ee
tonymtz <atony.mtz@gmail.com>
2023-03-23 17:31:00 -0700

    SHO-58: Fonts and colors (#12)

--- app/theme.ts
+++ app/theme.ts
@@ -1,16 +1,44 @@
+// @deprecated
 const primary = '#7d4fd0';
+// @deprecated
 const primaryDarker = '#44247c';
+// @deprecated
 const secondary = '#67c77b';
+// @deprecated
 const secondaryDarker = '#27913c';
+
+const orange = '#f1774c';
+const yellow = '#f3b760';
+const green = '#35ae9f';
+const linen = '#f8eee0';
+const wheat = '#f2dab2';
+const black = '#222222';
+const gray = '#bdbdbd';
+const spanishGray = '#a39b98';
 
 export default {
   main: primary,
 
-  buttonPrimaryColor: primary,
-  buttonPrimaryDarkerColor: primaryDarker,
-  buttonPrimaryTextColor: 'white',
+  buttonPrimaryColor: primary, // @deprecated
+  buttonPrimaryDarkerColor: primaryDarker, // @deprecated
+  buttonPrimaryTextColor: 'white', // @deprecated
+  buttonSecondaryColor: secondary, // @deprecated
+  buttonSecondaryDarkerColor: secondaryDarker, // @deprecated
+  buttonSecondaryTextColor: 'white', // @deprecated
 
-  buttonSecondaryColor: secondary,
-  buttonSecondaryDarkerColor: secondaryDarker,
-  buttonSecondaryTextColor: 'white',
+  colors: {
+    primary: orange,
+    secondary: green,
+    tertiary: yellow,
+    background: linen,
+    backgroundDarker: wheat,
+    black,
+    gray,
+    grayDarker: spanishGray,
+  },
+
+  fonts: {
+    oswald: '"Oswald", sans-serif',
+    raleway: '"Raleway", sans-serif',
+  },
 };
