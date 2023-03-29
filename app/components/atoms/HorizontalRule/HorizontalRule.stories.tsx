9301030e1e59a752f897e838b4638a39f2b54f9b
Kevin Ochoa Guerrero <kevinochoa.dev@gmail.com>
2023-03-29 12:33:59 -0700

    SHO-34: Horizontal Rule (#18)
    * Add line component
    
    * Add explicit storybook variants and refactor component name
    
    * Add HorizontalRule component

---
+++ app/components/atoms/HorizontalRule/HorizontalRule.stories.tsx
@@ -0,0 +1,13 @@
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import {Hr} from './HorizontalRule.component';
+
+export default {
+  title: 'Atoms/HorizontalRule',
+  component: Hr,
+} as ComponentMeta<typeof Hr>;
+
+const Template: ComponentStory<typeof Hr> = () => (
+  <Hr />
+);
+
+export const HorizontalRule = Template.bind({});
