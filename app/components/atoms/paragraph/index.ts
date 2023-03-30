31aff934f68318dc81e4e1c53afaf51b1c4939f8
tonymtz <atony.mtz@gmail.com>
2023-03-29 20:06:57 -0700

    Code clean up

--- app/components/atoms/paragraph/index.ts
+++ app/components/atoms/paragraph/index.ts
@@ -1,1 +1,1 @@
-export * from './Paragraph.component';
+export * from './paragraph.component';
--- app/components/atoms/paragraph/paragraph.stories.tsx
+++ app/components/atoms/paragraph/paragraph.stories.tsx
@@ -1,6 +1,6 @@
 import {ComponentStory, ComponentMeta} from '@storybook/react';
 
-import {Paragraph} from './Paragraph.component';
+import {Paragraph} from './paragraph.component';
 
 export default {
   title: 'Atoms/Paragraph',
@@ -11,11 +11,11 @@ const Template: ComponentStory<typeof Paragraph> = (args) => (
   <Paragraph {...args} />
 );
 
-export const ParagraphDefault = Template.bind({});
-ParagraphDefault.args = {
-  children: 'Lorem ipsum dolor sit amet, conctetuer adipiscing elit volutpat. Hendrerit invulputatevelit esse  Utwisi enim ad minimveniam.',
+export const Default = Template.bind({});
+Default.args = {
+  children:
+    'Lorem ipsum dolor sit amet, conctetuer adipiscing elit volutpat. Hendrerit invulputatevelit esse  Utwisi enim ad minimveniam.',
   lineHeight: 1.5,
   bold: false,
   italic: false,
 };
-
