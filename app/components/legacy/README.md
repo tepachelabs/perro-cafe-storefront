fe3bbc47621074d5b625b1f71e53dff602c518e4
tonymtz <atony.mtz@gmail.com>
2023-03-31 16:58:41 -0700

    Removes storefront (#24)

--- app/components/legacy/README.md
+++
@@ -1,4 +0,0 @@
-# Components
-
-This project follows the [Atomic Design](https://bradfrost.com/blog/post/atomic-web-design/) methodology. It is a way to structure your components in a way that is easy to understand and maintain.
-
--- app/root.tsx
+++ app/root.tsx
@@ -4,49 +4,16 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useCatch,
   useLoaderData,
-  useMatches,
 } from '@remix-run/react';
+import type {Shop} from '@shopify/hydrogen/storefront-api-types';
 import {
-  ShopifySalesChannel,
-  Seo,
-  type SeoHandleFunction,
-} from '@shopify/hydrogen';
-import {Shop, Cart} from '@shopify/hydrogen/storefront-api-types';
-import {
-  defer,
   type LinksFunction,
   type MetaFunction,
   type LoaderArgs,
-  type AppLoadContext,
 } from '@shopify/remix-oxygen';
-import {ThemeProvider} from 'styled-components';
-import invariant from 'tiny-invariant';
 
-import {Layout} from '~/components/legacy';
-
-import {GenericError} from './components/legacy/GenericError';
-import {NotFound} from './components/legacy/NotFound';
-
-// import styles from './styles/app.css';
-
-import {useAnalytics} from './hooks/useAnalytics';
-import {DEFAULT_LOCALE, parseMenu, type EnhancedMenu} from './lib/utils';
-import theme from './theme';
 import favicon from '../public/favicon.svg';
-
-const seo: SeoHandleFunction<typeof loader> = ({data, pathname}) => ({
-  title: data?.layout?.shop?.name,
-  titleTemplate: '%s | Hydrogen Demo Store',
-  description: data?.layout?.shop?.description,
-  handle: '@shopify',
-  url: `https://hydrogen.shop${pathname}`,
-});
-
-export const handle = {
-  seo,
-};
 
 export const links: LinksFunction = () => {
   return [
@@ -58,20 +25,7 @@ export const links: LinksFunction = () => {
       rel: 'preconnect',
       href: 'https://shop.app',
     },
-    {
-      rel: 'preconnect',
-      href: 'https://fonts.googleapis.com',
-    },
-    {
-      rel: 'preconnect',
-      href: 'https://fonts.gstatic.com',
-      crossorigin: true,
-    },
     {rel: 'icon', type: 'image/svg+xml', href: favicon},
-    {
-      rel: 'stylesheet',
-      href: 'https://fonts.googleapis.com/css2?family=Oswald:wght@700&family=Raleway:wght@400;700&display=swap',
-    },
   ];
 };
 
@@ -81,99 +35,26 @@ export const meta: MetaFunction = () => ({
 });
 
 export async function loader({context}: LoaderArgs) {
-  const [cartId, layout] = await Promise.all([
-    context.session.get('cartId'),
-    getLayoutData(context),
-  ]);
-
-  return defer({
-    layout,
-    selectedLocale: context.storefront.i18n,
-    cart: cartId ? getCart(context, cartId) : undefined,
-    analytics: {
-      shopifySalesChannel: ShopifySalesChannel.hydrogen,
-      shopId: layout.shop.id,
-    },
-  });
+  const layout = await context.storefront.query<{shop: Shop}>(LAYOUT_QUERY);
+  return {layout};
 }
 
 export default function App() {
   const data = useLoaderData<typeof loader>();
-  const locale = data.selectedLocale ?? DEFAULT_LOCALE;
-  const hasUserConsent = true;
 
-  useAnalytics(hasUserConsent, locale);
+  const {name} = data.layout.shop;
 
   return (
-    <ThemeProvider theme={theme}>
-      <html lang={locale.language}>
-        <head>
-          <Seo />
-          <Meta />
-          <Links />
-        </head>
-        <body>
-          <Layout
-            layout={data.layout as LayoutData}
-            key={`${locale.language}-${locale.country}`}
-          >
-            <Outlet />
-          </Layout>
-          <ScrollRestoration />
-          <Scripts />
-        </body>
-      </html>
-    </ThemeProvider>
-  );
-}
-
-export function CatchBoundary() {
-  const [root] = useMatches();
-  const caught = useCatch();
-  const isNotFound = caught.status === 404;
-  const locale = root.data?.selectedLocale ?? DEFAULT_LOCALE;
-
-  return (
-    <html lang={locale.language}>
+    <html lang="en">
       <head>
-        <title>{isNotFound ? 'Not found' : 'Error'}</title>
         <Meta />
         <Links />
       </head>
       <body>
-        <Layout
-          layout={root?.data?.layout}
-          key={`${locale.language}-${locale.country}`}
-        >
-          {isNotFound ? (
-            <NotFound type={caught.data?.pageType} />
-          ) : (
-            <GenericError
-              error={{message: `${caught.status} ${caught.data}`}}
-            />
-          )}
-        </Layout>
-        <Scripts />
-      </body>
-    </html>
-  );
-}
-
-export function ErrorBoundary({error}: {error: Error}) {
-  const [root] = useMatches();
-  const locale = root?.data?.selectedLocale ?? DEFAULT_LOCALE;
-
-  return (
-    <html lang={locale.language}>
-      <head>
-        <title>Error</title>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Layout layout={root?.data?.layout}>
-          <GenericError error={error} />
-        </Layout>
+        <h1>Hello, {name}</h1>
+        <p>This is a custom storefront powered by Hydrogen</p>
+        <Outlet />
+        <ScrollRestoration />
         <Scripts />
       </body>
     </html>
@@ -181,212 +62,10 @@ export function ErrorBoundary({error}: {error: Error}) {
 }
 
 const LAYOUT_QUERY = `#graphql
-  query layoutMenus(
-    $language: LanguageCode
-    $headerMenuHandle: String!
-    $footerMenuHandle: String!
-  ) @inContext(language: $language) {
+  query layout {
     shop {
-      id
       name
       description
     }
-    headerMenu: menu(handle: $headerMenuHandle) {
-      id
-      items {
-        ...MenuItem
-        items {
-          ...MenuItem
-        }
-      }
-    }
-    footerMenu: menu(handle: $footerMenuHandle) {
-      id
-      items {
-        ...MenuItem
-        items {
-          ...MenuItem
-        }
-      }
-    }
-  }
-  fragment MenuItem on MenuItem {
-    id
-    resourceId
-    tags
-    title
-    type
-    url
   }
 `;
-
-export interface LayoutData {
-  headerMenu: EnhancedMenu;
-  footerMenu: EnhancedMenu;
-  shop: Shop;
-  cart?: Promise<Cart>;
-}
-
-async function getLayoutData({storefront}: AppLoadContext) {
-  const HEADER_MENU_HANDLE = 'main-menu';
-  const FOOTER_MENU_HANDLE = 'footer';
-
-  const data = await storefront.query<LayoutData>(LAYOUT_QUERY, {
-    variables: {
-      headerMenuHandle: HEADER_MENU_HANDLE,
-      footerMenuHandle: FOOTER_MENU_HANDLE,
-      language: storefront.i18n.language,
-    },
-  });
-
-  invariant(data, 'No data returned from Shopify API');
-
-  /*
-    Modify specific links/routes (optional)
-    @see: https://shopify.dev/api/storefront/unstable/enums/MenuItemType
-    e.g here we map:
-      - /blogs/news -> /news
-      - /blog/news/blog-post -> /news/blog-post
-      - /collections/all -> /products
-  */
-  const customPrefixes = {BLOG: '', CATALOG: 'products'};
-
-  const headerMenu = data?.headerMenu
-    ? parseMenu(data.headerMenu, customPrefixes)
-    : undefined;
-
-  const footerMenu = data?.footerMenu
-    ? parseMenu(data.footerMenu, customPrefixes)
-    : undefined;
-
-  return {shop: data.shop, headerMenu, footerMenu};
-}
-
-const CART_QUERY = `#graphql
-  query CartQuery($cartId: ID!, $country: CountryCode, $language: LanguageCode)
-    @inContext(country: $country, language: $language) {
-    cart(id: $cartId) {
-      ...CartFragment
-    }
-  }
-
-  fragment CartFragment on Cart {
-    id
-    checkoutUrl
-    totalQuantity
-    buyerIdentity {
-      countryCode
-      customer {
-        id
-        email
-        firstName
-        lastName
-        displayName
-      }
-      email
-      phone
-    }
-    lines(first: 100) {
-      edges {
-        node {
-          id
-          quantity
-          attributes {
-            key
-            value
-          }
-          cost {
-            totalAmount {
-              amount
-              currencyCode
-            }
-            amountPerQuantity {
-              amount
-              currencyCode
-            }
-            compareAtAmountPerQuantity {
-              amount
-              currencyCode
-            }
-          }
-          merchandise {
-            ... on ProductVariant {
-              id
-              availableForSale
-              compareAtPrice {
-                ...MoneyFragment
-              }
-              price {
-                ...MoneyFragment
-              }
-              requiresShipping
-              title
-              image {
-                ...ImageFragment
-              }
-              product {
-                handle
-                title
-                id
-              }
-              selectedOptions {
-                name
-                value
-              }
-            }
-          }
-        }
-      }
-    }
-    cost {
-      subtotalAmount {
-        ...MoneyFragment
-      }
-      totalAmount {
-        ...MoneyFragment
-      }
-      totalDutyAmount {
-        ...MoneyFragment
-      }
-      totalTaxAmount {
-        ...MoneyFragment
-      }
-    }
-    note
-    attributes {
-      key
-      value
-    }
-    discountCodes {
-      code
-    }
-  }
-
-  fragment MoneyFragment on MoneyV2 {
-    currencyCode
-    amount
-  }
-
-  fragment ImageFragment on Image {
-    id
-    url
-    altText
-    width
-    height
-  }
-`;
-
-export async function getCart({storefront}: AppLoadContext, cartId: string) {
-  invariant(storefront, 'missing storefront client in cart query');
-
-  const {cart} = await storefront.query<{cart?: Cart}>(CART_QUERY, {
-    variables: {
-      cartId,
-      country: storefront.i18n.country,
-      language: storefront.i18n.language,
-    },
-    cache: storefront.CacheNone(),
-  });
-
-  return cart;
-}
