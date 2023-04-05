bcfecae2cdcfad986964379141b4e908e743ed14
kevstrosky <kevinochoa.dev@gmail.com>
2023-04-05 00:37:30 -0700

    Fixed problem that SVG  are not displayed. The <Hr /> component was replaced by a border-top

--- app/components/molecules/section-info/section-info.component.tsx
+++ app/components/molecules/section-info/section-info.component.tsx
@@ -1,53 +1,52 @@
 import {FC} from 'react';
 
+import events from './img/events.svg';
+import games from './img/games.svg';
+import ingredients from './img/ingredients.svg';
+import internet from './img/internet.svg';
 import {LeadingIcon, Props, StyledSectionInfo} from './section-info.styles';
-import {Hr} from '../../atoms/hr';
 import {Paragraph} from '../../atoms/paragraph';
 
 export const InternetInfo = () => {
   return (
-    <StyledSectionInfo>
-      <LeadingIcon
-        src={`static/media/app/components/molecules/section-info/img/internet.svg`}
-      />
-      <Hr />
-      <Paragraph bold>Internet Rápido</Paragraph>
-    </StyledSectionInfo>
+    <>
+      <LeadingIcon src={internet} />
+      <StyledSectionInfo>
+        <Paragraph bold>Internet Rápido</Paragraph>
+      </StyledSectionInfo>
+    </>
   );
 };
 
 export const IngredientsInfo = () => {
   return (
-    <StyledSectionInfo>
-      <LeadingIcon
-        src={`static/media/app/components/molecules/section-info/img/ingredients.svg`}
-      />
-      <Hr />
-      <Paragraph bold>Ingredientes 100% naturales</Paragraph>
-    </StyledSectionInfo>
+    <>
+      <LeadingIcon src={ingredients} />
+      <StyledSectionInfo>
+        <Paragraph bold>Ingredientes 100% naturales</Paragraph>
+      </StyledSectionInfo>
+    </>
   );
 };
 
 export const GamesInfo = () => {
   return (
-    <StyledSectionInfo>
-      <LeadingIcon
-        src={`static/media/app/components/molecules/section-info/img/games.svg`}
-      />
-      <Hr />
-      <Paragraph bold>Juegos de mesa</Paragraph>
-    </StyledSectionInfo>
+    <>
+      <LeadingIcon src={games} />
+      <StyledSectionInfo>
+        <Paragraph bold>Juegos de mesa</Paragraph>
+      </StyledSectionInfo>
+    </>
   );
 };
 
 export const EventsInfo = () => {
   return (
-    <StyledSectionInfo>
-      <LeadingIcon
-        src={`static/media/app/components/molecules/section-info/img/events.svg`}
-      />
-      <Hr />
-      <Paragraph bold>Hosteo de eventos</Paragraph>
-    </StyledSectionInfo>
+    <>
+      <LeadingIcon src={events} />
+      <StyledSectionInfo>
+        <Paragraph bold>Hosteo de eventos</Paragraph>
+      </StyledSectionInfo>
+    </>
   );
 };
--- app/components/molecules/section-info/section-info.styles.tsx
+++ app/components/molecules/section-info/section-info.styles.tsx
@@ -2,15 +2,17 @@ import {ReactNode} from 'react';
 import styled from 'styled-components';
 
 export interface Props {
-  variant: 'internet' | 'events' | 'ingredients' | 'games';
   children: ReactNode;
 }
 
 export const LeadingIcon = styled.img`
   width: 50px;
+  margin-bottom: 5px;
 `;
 
 export const StyledSectionInfo = styled.div`
+  border-top: 5px solid ${({theme}) => theme.colors.black};
+  padding-top: 5px;
   position: relative;
   display: flex;
   flex-direction: column;
