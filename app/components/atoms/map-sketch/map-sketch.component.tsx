eebbaaae57812e49282a4e762dbd0369c0aa9648
Kevin Ochoa Guerrero <kevinochoa.dev@gmail.com>
2023-04-06 18:15:32 -0700

    SHO-59: Map (#34)
    * Add map component
    
    * add index.ts
    
    * Rename <Map /> to <MapSketch />. Rename the directory and files. Rename MapContainer to MapSketchContainer
    
    * remove unused import declaration

---
+++ app/components/atoms/map-sketch/map-sketch.component.tsx
@@ -0,0 +1,12 @@
+import hand from './img/hand.svg';
+import map from './img/map.svg';
+import {MapSketchContainer, HandOverlay, MapImage} from './map-sketch.styles';
+
+export const MapSketch = () => {
+  return (
+    <MapSketchContainer>
+      <HandOverlay src={hand} />
+      <MapImage src={map} />
+    </MapSketchContainer>
+  );
+};
