6f5081efde9fb36a0e38e5b35bff8d553dfab11e
saulprl <phasecross@hotmail.com>
2023-04-28 16:48:19 -0700

    Add alt to subtitle icons

--- app/components/molecules/subtitle/subtitle.component.tsx
+++ app/components/molecules/subtitle/subtitle.component.tsx
@@ -6,21 +6,26 @@ import menu from './img/menu.svg';
 import temple from './img/temple.svg';
 import {Text, Icon, Frame} from './subtitle.styles';
 
+interface Icon {
+  src: string;
+  alt: string;
+}
+
 export interface TitleSectionProps {
   children?: ReactNode;
   icon?: 'community' | 'cult' | 'menu' | 'temple';
 }
 
-const icons = {
-  cult,
-  menu,
-  temple,
-  community,
+const icons: Record<string, Icon> = {
+  cult: {src: cult, alt: 'Ícono Culto'},
+  menu: {src: menu, alt: 'Ícono Menú'},
+  temple: {src: temple, alt: 'Ícono Templo'},
+  community: {src: community, alt: 'Ícono Comunidad'},
 };
 
 export const Subtitle: FC<TitleSectionProps> = ({icon, children}) => (
   <Frame>
-    {icon && <Icon src={icons[icon]} />}
+    {icon && <Icon src={icons[icon].src} alt={icons[icon].alt} />}
     <Text>{children}</Text>
   </Frame>
 );
