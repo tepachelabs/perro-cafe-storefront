b7376c78e1fbab972b3065e2f37581e02773847b
Saúl Ramos <saulramos378@gmail.com>
2023-05-02 18:44:08 -0700

    SHO-78: Information page Hero (#48)
    * Add RegularsHero component, add '/frecuentes' route
    
    * Remove hardcoded border width

---
+++ app/components/organisms/regulars-hero/regulars-hero.styles.tsx
@@ -0,0 +1,95 @@
+import styled, {css} from 'styled-components';
+
+import {select} from '../../../utils';
+import {Paragraph} from '../../atoms/paragraph';
+
+const desktopMediaQuery = css`
+  ${select(({mediaQueries}) => mediaQueries.desktop)}
+`;
+
+export const RegularsHeroContainer = styled.header`
+  display: flex;
+  max-width: ${select(({sizes}) => sizes.maxWidth)};
+  margin: 1rem auto 0;
+  flex-direction: column;
+
+  ${desktopMediaQuery} {
+    flex-direction: row;
+  }
+`;
+
+export const PrimaryContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
+  background-color: transparent;
+  border-bottom: ${select(({sizes}) => sizes.borderWidth)} solid
+    ${select(({colors}) => colors.black)};
+
+  ${desktopMediaQuery} {
+    flex: 1.3;
+  }
+`;
+
+export const Description = styled(Paragraph)`
+  display: block;
+
+  background-color: ${select(({colors}) => colors.black)};
+  color: ${select(({colors}) => colors.background)};
+
+  padding: 1.5rem;
+
+  ${desktopMediaQuery} {
+    padding: 1.5rem 5.5rem;
+  }
+`;
+
+export const SecondaryContainer = styled.div`
+  position: relative;
+  border-bottom: ${select(({sizes}) => sizes.borderWidth)} solid
+    ${select(({colors}) => colors.black)};
+
+  ${desktopMediaQuery} {
+    flex: 1;
+    border: ${select(({sizes}) => sizes.borderWidth)} solid
+      ${select(({colors}) => colors.black)};
+  }
+`;
+
+export const RegularsTitle = styled.h1`
+  display: block;
+
+  color: ${select(({colors}) => colors.black)};
+  font-family: ${select(({fonts}) => fonts.title)};
+  font-size: 44px;
+  text-transform: uppercase;
+  font-weight: 400;
+
+  padding: 1.5rem;
+
+  ${desktopMediaQuery} {
+    padding: 3rem 1.5rem;
+  }
+
+  &:after {
+    border-bottom: ${select(({sizes}) => sizes.borderWidth)} solid
+      ${select(({colors}) => colors.black)};
+    content: ' ';
+    display: block;
+    height: 4px;
+    position: absolute;
+    width: 156px;
+  }
+`;
+
+export const RegularsHeroBanner = styled.img`
+  display: block;
+
+  width: 100%;
+
+  ${desktopMediaQuery} {
+    height: 100%;
+    object-fit: cover;
+  }
+`;
