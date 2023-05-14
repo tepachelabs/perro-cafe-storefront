351c8c3ec20f87da6c8b0dbfb963fa76dfb5b0b8
tonymtz <atony.mtz@gmail.com>
2023-05-13 18:24:10 -0700

    fixes arrow-button.styles.tsx

--- app/components/atoms/arrow-button/arrow-button.styles.tsx
+++ app/components/atoms/arrow-button/arrow-button.styles.tsx
@@ -1,11 +1,10 @@
-import styled, {css} from 'styled-components';
+import {css, Theme} from '@emotion/react';
+import styled from '@emotion/styled';
 
 import arrowImg from './img/arrow.svg';
-import {select} from '../../../utils';
 
-const ButtonBaseStyle = css`
-  border: ${select(({sizes}) => sizes.borderWidth)} solid
-    ${select(({colors}) => colors.black)};
+const ButtonBaseStyle = ({theme}: {theme: Theme}) => css`
+  border: ${theme.sizes.borderWidth} solid ${theme.colors.black};
   height: 48px;
   width: 48px;
 `;
@@ -15,7 +14,7 @@ export const ArrowButton = styled.button<{
 }>`
   ${ButtonBaseStyle};
 
-  background-color: ${select(({colors}) => colors.backgroundDarker)};
+  background-color: ${(props) => props.theme.colors.backgroundDarker};
   position: relative;
 
   &:before {
@@ -23,30 +22,32 @@ export const ArrowButton = styled.button<{
 
     content: ' ';
     display: block;
-    left: -${select(({sizes}) => sizes.borderWidth)}; // compensate for border in parent
-    left: -${select(({sizes}) => sizes.borderWidth)}; // compensate for border in parent
+    left: -${(props) => props.theme.sizes.borderWidth}; // compensate for border in parent
+    left: -${(props) => props.theme.sizes.borderWidth}; // compensate for border in parent
     position: absolute;
-    top: -${select(({sizes}) => sizes.borderWidth)}; // compensate for border in parent
+    top: -${(props) => props.theme.sizes.borderWidth}; // compensate for border in parent
     transition: all 200ms ease;
     transform: translateY(
-      calc(${select(({sizes}) => sizes.borderWidth)} * -2.5)
+      calc(${(props) => props.theme.sizes.borderWidth} * -2.5)
     );
     will-change: transform;
 
-    background: ${select(({colors}) => colors.background)} url(${arrowImg})
+    background: ${(props) => props.theme.colors.background} url(${arrowImg})
       no-repeat center;
     background-size: 50% 50%;
   }
 
   &:hover:before {
     filter: brightness(1.05);
-    transform: translateY(calc(${select(({sizes}) => sizes.borderWidth)} * -3));
+    transform: translateY(
+      calc(${(props) => props.theme.sizes.borderWidth} * -3)
+    );
   }
 
   &:active:before {
     filter: brightness(0.95);
     transform: translateY(
-      calc(${select(({sizes}) => sizes.borderWidth)} * -1.5)
+      calc(${(props) => props.theme.sizes.borderWidth} * -1.5)
     );
   }
 
