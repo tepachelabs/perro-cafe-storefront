2aa5db616b892e54fb1d0cbe04ebeb3f167ffd5d
saulprl <phasecross@hotmail.com>
2023-05-16 09:42:12 -0700

    Fix utils.ts

--- app/utils.ts
+++ app/utils.ts
@@ -1,8 +1,7 @@
-import {MainTheme} from '~/theme';
+import {Theme} from '@emotion/react';
 
-export const select =
-  (fn: (_: MainTheme) => string) => (props: {theme: MainTheme}) =>
-    fn(props.theme);
+export const select = (fn: (_: Theme) => string) => (props: {theme: Theme}) =>
+  fn(props.theme);
 
 export const currencyFormatter = Intl.NumberFormat('es-MX', {
   style: 'currency',
