b3b248eb7b828b5efda8f6c2c03a7c0bcb06c3c5
saulprl <phasecross@hotmail.com>
2023-05-19 17:12:15 -0700

    Add query to get a specific type of Metaobjects from Shopify to show on /frecuentes

--- app/components/templates/regulars-info/regulars-info.component.tsx
+++ app/components/templates/regulars-info/regulars-info.component.tsx
@@ -1,3 +1,5 @@
+import {FC} from 'react';
+
 import {
   BlockDivider,
   Grid,
@@ -11,21 +13,32 @@ import {Divider} from '../../atoms/divider';
 import {Paragraph} from '../../atoms/paragraph';
 import {Subtitle} from '../../molecules/subtitle';
 
-const {regularsInfo, regularsDisclaimer} = configData;
+const {regularsDisclaimer} = configData;
 
-export const RegularsInfo = () => {
+export interface RegularsInfoBlock {
+  index?: string;
+  title?: string;
+  body?: string;
+  footer?: string;
+}
+
+interface Props {
+  infoBlocks: RegularsInfoBlock[];
+}
+
+export const RegularsInfo: FC<Props> = ({infoBlocks}) => {
   const isDesktop = useMediaQuery('(min-width: 768px)');
 
   return (
     <InfoSection>
       <Grid>
-        {regularsInfo.map((info, index, arr) => {
+        {infoBlocks.map((info, index, arr) => {
           const renderBlockDivider = isDesktop
             ? index < arr.length - (2 - (arr.length % 2))
             : index < arr.length - 1;
 
           return (
-            <InfoBlock key={info.id}>
+            <InfoBlock key={info.title}>
               <Subtitle numeral={`${index + 1}.`}>{info.title}</Subtitle>
               <Paragraph>{info.body}</Paragraph>
               <Paragraph bold>{info.footer}</Paragraph>
--- app/routes/frecuentes.tsx
+++ app/routes/frecuentes.tsx
@@ -1,27 +1,79 @@
-import {Link} from '@remix-run/react';
+import {Link, useLoaderData} from '@remix-run/react';
+import {
+  Metaobject,
+  MetaobjectField,
+} from '@shopify/hydrogen/storefront-api-types';
+import {LoaderArgs, json} from '@shopify/remix-oxygen';
 
 import {NavBar, NavBarLink} from '~/components/organisms/navbar';
 import {RegularsHero} from '~/components/organisms/regulars-hero/regulars-hero.component';
 import {Footer} from '~/components/templates/footer';
-import {RegularsInfo} from '~/components/templates/regulars-info';
+import {
+  RegularsInfo,
+  RegularsInfoBlock,
+} from '~/components/templates/regulars-info';
 import configData from '~/config.json';
+
+type ReducedMetafield = Record<keyof RegularsInfoBlock, string | undefined>;
+
+export async function loader({context: {storefront}}: LoaderArgs) {
+  const {metaobjects} = await storefront.query<Array<Metaobject>>(
+    METAOBJECTS_QUERY,
+  );
+
+  return json({metaobjects});
+}
 
 // @ts-ignore
 const _Link = (props) => <NavBarLink {...props} as={Link} />;
 
 export default function Frecuentes() {
+  const {
+    metaobjects: {nodes: metaobjectNodes},
+  } = useLoaderData<typeof loader>();
+
   const links = configData.navbar.links.map((link) => ({
     label: link.label,
     href: link.link,
     ...(link.label === 'Frecuentes' && {active: 'true'}),
   }));
 
+  const infoBlocks = (metaobjectNodes as Array<Metaobject>)
+    .map<RegularsInfoBlock>((metaobject) => {
+      const fields = metaobject.fields.reduce(
+        (result: ReducedMetafield, {key, value}) => {
+          result[key as keyof RegularsInfoBlock] = value ?? '';
+
+          return result;
+        },
+        {} as ReducedMetafield,
+      );
+
+      return fields;
+    })
+    .sort(({index: indexA}, {index: indexB}) => +indexA! - +indexB!);
+
   return (
     <>
       <NavBar links={links} linkRender={_Link} />
       <RegularsHero />
-      <RegularsInfo />
+      <RegularsInfo infoBlocks={infoBlocks} />
       <Footer />
     </>
   );
 }
+
+const METAOBJECTS_QUERY = `#graphql
+  query MetaObjects {
+    metaobjects(first: 4, type: "regulars_info") {
+      nodes {
+        id,
+        type,
+        fields {
+          key,
+          value
+        }
+      }
+    }
+  }
+`;
