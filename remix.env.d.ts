7f1de88ac60ab997882d709d41085e20a962a6c7
tonymtz <atony.mtz@gmail.com>
2023-05-23 22:54:51 -0700

    integrates rollbar service (#59)

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -22,6 +22,7 @@ declare global {
     PUBLIC_STORE_DOMAIN: string;
     PUBLIC_STOREFRONT_ID: string;
     PUBLIC_POSTHOG_KEY: string;
+    PUBLIC_ROLLBAR_KEY: string;
     NODE_ENV: string;
   }
 }
