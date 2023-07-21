dcda68ce38df6e86b3afe6038860be352dc146d4
saulprl <phasecross@hotmail.com>
2023-07-21 13:00:22 -0700

    Start Storybook migration from 6.5.16 to 7.1.0

---
+++ .storybook/preview.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import {Preview} from '@storybook/react';
+import {ThemeProvider} from '@emotion/react';
+
+import {GlobalStyles} from '../app/global.styles';
+import theme from '../app/theme';
+
+const preview: Preview = {
+  parameters: {
+    actions: {argTypesRegex: '^on[A-Z].*'},
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default preview;
+
+// export const parameters = {
+//   actions: {argTypesRegex: '^on[A-Z].*'},
+//   controls: {
+//     matchers: {
+//       color: /(background|color)$/i,
+//       date: /Date$/,
+//     },
+//   },
+// };
+
+// export const decorators = [
+//   (Story) => (
+//     <ThemeProvider theme={theme}>
+//       <GlobalStyles />
+//       <Story />
+//     </ThemeProvider>
+//   ),
+// ];
